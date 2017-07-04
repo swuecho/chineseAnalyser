0254a7dc6c4ad3f840cdacf71fc8d25563382d4e
Hao Wu <echowuhao@gmail.com>
2017-07-04 19:16:43 +0000

    add code

--- lib/LucyX/Analysis/Analyzer/Chinese.pm
+++ lib/LucyX/Analysis/Analyzer/Chinese.pm
@@ -1,11 +1,44 @@
 package LucyX::Analysis::Analyzer::Chinese;
-use 5.008001;
-use strict;
-use warnings;
 
 our $VERSION = "0.01";
 
+use v5.10;
+use Encode qw(decode_utf8);
+use Lingua::ZH::Jieba;
 
+use base qw( Lucy::Analysis::Analyzer );
+
+my $jieba = Lingua::ZH::Jieba->new;
+
+sub new {
+    my $self = shift->SUPER::new;
+    return $self;
+}
+
+sub equals {
+    my ( $self, $other ) = @_;
+    return $other->isa(__PACKAGE__);
+}
+
+sub transform {
+    my ($self, $inversion)= @_;
+    return $inversion;
+}
+
+sub transform_text {
+    my ($self, $text) = @_;
+    my $inversion = Lucy::Analysis::Inversion->new;
+    my $tokens = $jieba->cut_for_search_ex(decode_utf8($text));
+    $inversion->append(
+       Lucy::Analysis::Token->new(text =>$_->[0],
+                                  start_offset=> $_->[1] ,
+                                  # end_offset + start_offset + size
+                                  end_offset=> $_->[1] + $_->[2] 
+        )
+        
+    ) for @$tokens;
+    return $inversion;
+}
 
 1;
 __END__
