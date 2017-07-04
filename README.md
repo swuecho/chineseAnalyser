593a6746afe3428f56ec119af60d549f0a2577f8
Hao Wu <echowuhao@gmail.com>
2017-07-04 19:12:34 +0000

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+LucyX::Analysis::Analyzer::Chinese - It's new $module
+
+# SYNOPSIS
+
+    use LucyX::Analysis::Analyzer::Chinese;
+
+# DESCRIPTION
+
+LucyX::Analysis::Analyzer::Chinese is ...
+
+# LICENSE
+
+Copyright (C) Hao Wu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hao Wu <echowuhao@gmail.com>
---
+++ lib/LucyX/Analysis/Analyzer/Chinese.pm
@@ -0,0 +1,39 @@
+package LucyX::Analysis::Analyzer::Chinese;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+LucyX::Analysis::Analyzer::Chinese - It's new $module
+
+=head1 SYNOPSIS
+
+    use LucyX::Analysis::Analyzer::Chinese;
+
+=head1 DESCRIPTION
+
+LucyX::Analysis::Analyzer::Chinese is ...
+
+=head1 LICENSE
+
+Copyright (C) Hao Wu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hao Wu E<lt>echowuhao@gmail.comE<gt>
+
+=cut
+
